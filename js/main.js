65cb7deb69733dde565520baa2ea6debc30283aa
AbdulrhmanElshahat <https://AbdulrhmanElshahat@github.com>
2022-11-11 19:11:54 +0200

    'ssss'

--- js/main.js
+++ js/main.js
@@ -1,16 +1,51 @@
 let menuBtn = $('header .fa-bars'),
     menu = $('header ul'),
-    menuItems = $('header ul li a');
+    menuItems = $('header ul li a'),
+    mainSections= $('body > div'),
+    thePathName = window.location.pathname;   
 // toggle menu
 menuBtn.click(function(){
     menu.slideToggle()
 })
-if(menuBtn.css('display') !== 'none'){
-    menuItems.click(function(){
-    menu.fadeOut()
-})
+
+if(thePathName ==='/' || thePathName === '/index.html'){
+    menuItems.click(function(e){
+        setTimeout(() => {
+            menuItems.removeClass('active')
+            $(this).addClass('active')
+            if(window.innerWidth<=768){
+                menu.slideUp()
+            }
+        }, 1000);
+    })
+    
+    $(window).scroll(function(){
+        let y = window.scrollY,
+            mainSectionsArr = Array.from(mainSections)
+                let z = mainSectionsArr.filter(e=> $(e).offset().top <= y && ($(e).offset().top + $(e).innerHeight()) >= y + 10)
+                menuItems.removeClass('active')
+              $(`a[href="#${$(z[0]).attr('id')}"`).addClass('active')
+    })
+
+    scrollInto(menuItems)
 }
-menuItems.click(function(){
-    menuItems.removeClass('active')
-    $(this).addClass('active')
-})
+if(thePathName === '/works.html'){
+    window.scrollTo(0 , $('.our-work').offset().top)
+}
+if(thePathName === '/service.html'){
+    window.scrollTo(0 , $('.our-services').offset().top)
+}
+
+
+function scrollInto(elements){
+    let arr = Array.from(elements)
+    arr.forEach(elem => {
+        elem.addEventListener('click' , e =>{
+            e.preventDefault()
+        document.querySelector(e.target.dataset.section).scrollIntoView({behavior:'smooth'})
+        })
+    })
+}
+
+
+
